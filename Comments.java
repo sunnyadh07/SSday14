28620ab62e672d1fb6393475a1a0501bac7861ba
Sunny Adhikari <sunnyadh07@gmail.com>
2021-04-09 22:43:40 -0700

    first commit

---
+++ Comments.java
@@ -0,0 +1,16 @@
+package sample;
+
+public class Comments {
+    /*
+    1. You can create pre-defined 3D shapes like boxes, cylinders, spheres.
+    2. In Javafx you can set various effects such a bloom, blur and glow. These effects are represented by a class and these classes are
+    available in a package names javafx.scene.effect.
+    3. Using JavaFX, you can apply transformations on nodes such as rotation, scaling and translation.
+    All these transformations are represented by various classes and these belong to the package javafx.scene.transform.
+    4. you can animate a node by changing its property over time using a package called javafx.animation.
+    5.You can load and modify images using the classes provided by JavaFX in the package javafx.scene.image.
+    JavaFX supports the image formats like Bmp, Gif, Jpeg, Png.
+    6. 
+
+     */
+}
